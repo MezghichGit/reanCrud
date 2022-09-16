d158599cced0b8500f44fdcc9f9de001910a5bef
moahmed amine mezghich <git@mezghich.com>
2022-09-16 14:29:22 +0100

    Update User complete

--- screens/UpdateUser.js
+++ screens/UpdateUser.js
@@ -7,8 +7,24 @@ const UpdateUser = ({ route, navigation }) => {
     //console.log(user);
     const [nom, setNom] = useState(user.nom);
     const [prenom, setPrenom] = useState(user.prenom);
-
-
+    let userUpdated = {
+        nom:nom,
+        prenom:prenom
+    }
+   const updateUserData = ()=> {
+        // Simple PUT request with a JSON body using fetch
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userUpdated)
+        };
+        fetch('https://pharma.tunitransport.com/api/public/index.php/api/pharma/users/'+user.id, requestOptions)
+            .then(response => {
+                response.json();
+                navigation.push('ListUsers')
+              })
+    }
+    
     return (
         <SafeAreaView>
             <Text style={styles.titreText}>Interface de mise à jour</Text>
@@ -25,7 +41,7 @@ const UpdateUser = ({ route, navigation }) => {
                 value={prenom}
 
             />
-            <Button title="Mise à jour" ></Button>
+            <Button title="Mise à jour" onPress={updateUserData}></Button>
         </SafeAreaView>
     );
 
